9d1af614e9c0d0a00206edfcb7497402bb39b071
William Tallarico <wct9667@rit.edu>
2024-06-21 15:53:06 -0400

    Fixed the @next/font error and updated linting

--- src/app/comic/create/publish/BranchPlacer.tsx
+++ src/app/comic/create/publish/BranchPlacer.tsx
@@ -1,16 +1,18 @@
 "use client";
 import '../../../../styles/publish.css'
 
+import Image from 'next/image'
+
 import {useState } from 'react';
 import { v4 as uuidv4 } from 'uuid'; //uuid library 
-import {PanelSet, Branch} from "./PanelSet";
+import {PanelSet} from "./PanelSet";
 
 
 const BranchPlacer = () => {
     const [branCount, setBranch] = useState(0);
     const [adding, setAdding] = useState(false);
-    const [parId, setparId] = useState(" ");
-    const [curId, setCurId] = useState(parId ? parId : uuidv4());
+    const [parId] = useState(" ");
+    const [curId] = useState(parId ? parId : uuidv4());
 
     const image1Link = localStorage.getItem("image-1");
     const image2Link = localStorage.getItem("image-2");
@@ -134,13 +136,13 @@ const BranchPlacer = () => {
                 {/* temp vals for testing purposes, will be filled in with correct uploaded panels and vals through js */}
                 {/* uses placeholder class for images to be replaced with user uploaded images */}
                 <div className="carousel-item active">
-                    <img id="first-panel" onClick={() => { addBranchHook(1) }} src={image1Link? image1Link : "/images/previewPlaceholder.png"} className="d-block placeholder-" alt="..." width={400} height={200} />
+                    <Image id="first-panel" onClick={() => { addBranchHook(1) }} src={image1Link? image1Link : "/images/previewPlaceholder.png"} className="d-block placeholder-" alt="..." width={400} height={200} />
                 </div>
                 <div className="carousel-item">
-                    <img id="second-panel" onClick={() => { addBranchHook(2) }} src={image2Link? image2Link : "/images/previewPlaceholder.png"} className="d-block placeholder" alt="..." width={400} height={200} />
+                    <Image id="second-panel" onClick={() => { addBranchHook(2) }} src={image2Link? image2Link : "/images/previewPlaceholder.png"} className="d-block placeholder" alt="..." width={400} height={200} />
                 </div>
                 <div className="carousel-item" id="branch-hook-img" >
-                    <img id="third-panel" onClick={() => { addBranchHook(3) }} src={image3Link? image3Link : "/images/previewPlaceholder.png"} className="d-block placeholder" alt="..." width={400} height={200} useMap="#panel-map" />
+                    <Image id="third-panel" onClick={() => { addBranchHook(3) }} src={image3Link? image3Link : "/images/previewPlaceholder.png"} className="d-block placeholder" alt="..." width={400} height={200} useMap="#panel-map" />
                     {/* map of img containing clickable areas/sections defined by user*/}
                     <map name="panel-map">
                         {/* ex clickable area
